9335746424b6caae20afda94867f503ab5056156
zadabbas.s <zadabbas.s>
2021-02-28 12:13:04 +0330

    -

---
+++ src/main/java/ac/ttcu/common/Majors.java
@@ -0,0 +1,19 @@
+package ac.ttcu.common;
+
+public enum Majors {
+    SOFTWARE("مهندسی نرم افزار"),
+    ELECTRONICS("برق و الکترونیک"),
+    MECHANICS("مکانیک"),
+    ICT("مخابرات"),
+    ARCHITECTURE("معماری و نقشه کشی"),
+    GRAPHICS("هنر و گرافیک"),
+    ACCOUNTING("حسابداری"),
+    CLOTHING_DESIGN("طراحی دوخت"),
+    PHYSICAL_EDUCATION("طراحی دوخت"),
+    IT("فناوری اطلاعات");
+    public final String Majors;
+
+   private Majors(String majors) {
+        Majors = majors;
+    }
+}
