970382ce4437b3af6cb4d8973ac2c3dec164ffe2
samane-zad <zadabbassamane@gmail.com>
2021-07-18 21:34:52 +0430

    -

--- src/main/java/ac/ttcu/TtcuApplication.java
+++ src/main/java/ac/ttcu/TtcuApplication.java
@@ -4,7 +4,6 @@ package ac.ttcu;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
@@ -21,7 +20,7 @@ public class TtcuApplication implements InitializingBean {
 
 
     public static void main(String[] args)throws Exception {
-        ip=InetAddress.getLocalHost().getHostAddress().toString();
+        ip = InetAddress.getLocalHost().getHostAddress();
         SpringApplication ttcu=new SpringApplication(TtcuApplication.class);
         Environment env=ttcu.run(args).getEnvironment();
         port=env.getProperty("server.port");
--- src/main/java/ac/ttcu/controller/PostUploadResource.java
+++ src/main/java/ac/ttcu/controller/PostUploadResource.java
@@ -1,17 +1,17 @@
 package ac.ttcu.controller;
 
 import ac.ttcu.common.Message;
-import ac.ttcu.model.entity.dto.PostDTO;
 import ac.ttcu.model.service.dao.PostService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/api/teacher")
@@ -24,10 +24,13 @@ public class PostUploadResource {
     }
 
     @RequestMapping(value = "/uploadPost", method = RequestMethod.POST)
-    public ResponseEntity<Message> savePost(@RequestBody PostDTO postDTO, @RequestHeader HttpHeaders httpHeaders) {
+    public ResponseEntity<Message> savePost(@RequestParam("image") MultipartFile image,
+                                            @RequestHeader HttpHeaders httpHeaders) {
         Message message = null;
         try {
-            logger.info("Saving post: {}", postDTO);
+//            logger.info("Saving post: {}", postDTO);
+//            postDTO.setImage(image);
+//            postService.save(postDTO);
 
         } catch (Exception e) {
             e.printStackTrace();
