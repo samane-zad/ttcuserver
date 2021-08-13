c16d9da3082962161f2683063ef72c88642e4f8d
samane-zad <zadabbassamane@gmail.com>
2021-08-13 12:00:45 +0430

    -

--- src/main/java/ac/ttcu/model/entity/dto/Entity.java
+++ src/main/java/ac/ttcu/model/entity/dto/Entity.java
@@ -4,18 +4,35 @@ import java.io.Serializable;
 import java.lang.reflect.Field;
 
 public class Entity implements Serializable {
+    /**
+     * toString method for non ac.ttcu type objects
+     *
+     * @return designed string format presenting all object fields with values
+     */
     @Override
     public String toString() {
         try {
-            String out = this.getClass().getName() + "{";
+            String className = this.getClass().getName();
+            String out = className.substring(className.lastIndexOf(".") + 1);
             StringBuilder stringBuilder = new StringBuilder(out);
+            stringBuilder.append('{');
             for (Field field : this.getClass().getDeclaredFields()) {
                 field.setAccessible(true);
-                stringBuilder.append(field.getName() + "='" + field.get(this) + "',");
+                String value;
+                try {
+                    value = field.get(this).toString();
+                    if (field.getType().getName().startsWith("ac.ttcu")) {
+                        value = field.get(this).toString();
+                    }
+                } catch (NullPointerException ne) {
+                    value = null;
+                }
+
+                stringBuilder.append(field.getName() + "='" + value + "',");
             }
-            stringBuilder.deleteCharAt(out.length() - 1);
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
             stringBuilder.append('}');
-            return out;
+            return stringBuilder.toString();
         } catch (Exception e) {
             return null;
         }
